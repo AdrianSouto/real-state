6823532629612c4aadaf41eecb8fadb3b2c2b62a
Adrian <adrianalejandrosm03@gmail.com>
2025-06-12 22:42:21 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+const dimensions = {
+    rows: 5,
+    cols: 10
+}
+
+function App() {
+    const {rows, cols} = dimensions
+  return (
+    <div className={'h-dvh  w-full justify-center items-center'}>
+        {
+        Array.from({length: rows}).map((_, rowIndex) => (
+            <div className={`flex -translate-y-[${rowIndex * 2}px]`} key={rowIndex}>
+                {
+                    Array.from({length: cols}).map((_, colIndex) => (
+                        <div className={'flex'}>
+                            <div className={'w-1'}/>
+                            <div className={'bg-red-500 hexagon'} key={colIndex}></div>
+                        </div>
+
+                    ))
+                }
+            </div>
+        ))
+        }
+    </div>
+  )
+}
+
+export default App
